658ac18c68ce33aafa8fe23477e43e9175e491fd
fhorgeDevelopers <139235909+fhorgeDevelopers@users.noreply.github.com>
2023-07-24 22:03:25 +0100

    Update Calls.js

--- src/providers/Calls.js
+++ src/providers/Calls.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 import { useHook } from "./Hook";
 
 const CallsContext = createContext(null);
@@ -26,6 +26,9 @@ export const Calls = ({ children }) => {
     }
     }
 
+    useEffect(()=>{
+    getNavCourses()
+  },[])
     return (
         <CallsContext.Provider value={{ navCourses }}>
             {children}
