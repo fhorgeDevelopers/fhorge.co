2dd14b18c8685379e966ae961d85c9ca69549847
theCarrierOfGod <engrolaolumide@gmail.com>
2023-07-27 15:08:15 +0100

    trying something out

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Footer from '../components/Footer/Footer'
 import Navigation from '../components/Navigation/Navigation'
+import Hero from './Index'
 
 const Home = () => {
   return (
     <>
       <Navigation />
-
+      <Hero />
       <Footer />
     </>
   )
