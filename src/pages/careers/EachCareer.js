911f0a73c26a4d486faa434fd65b7c4cd3c76ebb
theCarrierOfGod <engrolaolumide@gmail.com>
2023-07-29 20:45:45 +0100

    jay

--- src/pages/careers/EachCareer.js
+++ src/pages/careers/EachCareer.js
@@ -1,27 +1,33 @@
 import React, { useEffect } from 'react'
 import { useParams } from 'react-router'
 import { useCalls } from '../../providers/Calls';
+import Footer from '../components/Footer/Footer';
 import Navigation from '../components/Navigation/Navigation'
 
 const EachCareer = () => {
-    let { category_id, career_id } = useParams();
-    const calls = useCalls();
+  let { category_id, career_id } = useParams();
+  const calls = useCalls();
 
-    useEffect(() => {
-      calls.getCareerDetail(category_id, career_id);
-    
-      return () => {
-        return career_id;
-      }
-    }, [career_id])
-    
+  useEffect(() => {
+    calls.getCareerDetail(category_id, career_id);
 
-    return (
-        <>
-            <Navigation />
-            {career_id}
-        </>
-    )
+    return () => {
+      return career_id;
+    }
+  }, [career_id])
+
+
+  return (
+    <>
+      <Navigation />
+      <main style={{ minHeight: '40vh' }}>
+        <h4>
+          {career_id}
+        </h4>
+      </main>
+      <Footer />
+    </>
+  )
 }
 
 export default EachCareer
