a0e8141154829cde9492aea7bdcdeb5d897ac4be
fhorgeDevelopers <139235909+fhorgeDevelopers@users.noreply.github.com>
2023-07-29 21:11:32 +0100

    Update Hero.js

--- src/pages/home/Hero.js
+++ src/pages/home/Hero.js
@@ -1,14 +1,14 @@
 // Components
-import Intro from "../home/landing-page/Intro";
-import Categories from "../home/landing-page/Categories";
+import Intro from "./landing-page/Intro";
+import Categories from "./landing-page/Categories";
 import Goals from "./landing-page/Goals";
 import Courses from "./landing-page/Courses";
 
 // Styles
-import "../../css/homepage.css";
-import "../../css/homepage2.css";
-import "../../css/homepage-lower.css";
-import "../../css/iconfont.css";
+//import "../../css/homepage.css";
+//import "../../css/homepage2.css";
+//import "../../css/homepage-lower.css";
+//import "../../css/iconfont.css";
 
 const Hero = () => {
     return (
