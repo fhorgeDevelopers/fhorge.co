4991fb1388609e5bbc94ee982561d8f35ef5f0da
theCarrierOfGod <engrolaolumide@gmail.com>
2023-08-15 19:21:30 +0100

    authentication modal

--- src/pages/home/landing-page/Intro.js
+++ src/pages/home/landing-page/Intro.js
@@ -2,16 +2,25 @@ import { useState } from "react";
 import Switch from "../Switch";
 
 import Stats from "./Stats";
+import { useNavigate } from "react-router";
 const Intro = () => {
-    const [searchText, setSearchText] = useState('')
+    const [searchText, setSearchText] = useState('');
+    const navigate = useNavigate();
+
+    const searchNow = (e) => {
+        e.preventDefault();
+        navigate(`/courses/query/${searchText}`)
+    }
     return (
         <div className="intro">
             <div className="inner">
                 <Switch />
                 <h1>Free Online Courses With Certificates & Diplomas</h1>
-                <form id="home-header-search" className="home-search" method="GET" action="" accept-charset="UTF-8" autocomplete="off">
-                    <input maxlength="200" id="autocomplete" className="autocomplete_field" name="query" type="text" value={searchText} />
-                    <span><span className="icon-search2"></span> What do you want to learn today? </span>
+                <form id="home-header-search" className="home-search" method="GET" action="" accept-charset="UTF-8" autocomplete="off" onSubmit={(e) => searchNow(e)}>
+                    <input maxlength="200" id="autocomplete" className="autocomplete_field" name="query" onChange={(e) => setSearchText(e.target.value)} type="text" value={searchText} />
+                    <span>
+                        <span className="icon-search2"></span> What do you want to learn today?
+                    </span>
                 </form>
                 <div className="intro__images">
                     <span className="p-2 mb-4">
