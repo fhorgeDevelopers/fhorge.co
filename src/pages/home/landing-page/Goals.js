a019cb0019bc496ce9db9941fe1f5f71b00ce565
Okiki Areo <okikiareo@gmail.com>
2023-08-19 18:44:53 +0100

    update

--- src/pages/home/landing-page/Goals.js
+++ src/pages/home/landing-page/Goals.js
@@ -7,13 +7,13 @@ const Goals = () => {
                 <h2>Not sure where to begin? <span className="hide-on-mobile">What is your main goal on Fhorge?</span></h2>
                 <h5 className="hide-on-desktop">What is your main goal on Fhorge?</h5>
                 <div className="goals__container">
-                    <Link to="/user/questionnaire?edit=false&goal=1" className="goal lazyload" data-bg="https://cdn01.alison-static.net/public/html/site/img/homepage/flag.svg"> I want to <br /><span>upskill</span>
+                    <Link to="/user/questionnaire?edit=false&goal=1" className="goal goal1 lazyload" data-bg="https://cdn01.alison-static.net/public/html/site/img/homepage/flag.svg"> I want to <br /><span>upskill</span>
                         <span className="arrow icon-carousel-arrow"></span>
                     </Link>
-                    <Link to="/user/questionnaire?edit=false&goal=2" className="goal lazyload" data-bg="https://cdn01.alison-static.net/public/html/site/img/homepage/signpost.svg"> I want to <br /><span>change careers</span>
+                    <Link to="/user/questionnaire?edit=false&goal=2" className="goal goal2 lazyload" data-bg="https://cdn01.alison-static.net/public/html/site/img/homepage/signpost.svg"> I want to <br /><span>change careers</span>
                         <span className="arrow icon-carousel-arrow"></span>
                     </Link>
-                    <Link to="/user/questionnaire?edit=false&goal=3" className="goal lazyload" data-bg="https://cdn01.alison-static.net/public/html/site/img/homepage/jigsaw.svg"> I want to explore <br /><span>hobbies & interests</span>
+                    <Link to="/user/questionnaire?edit=false&goal=3" className="goal goal3 lazyload" data-bg="https://cdn01.alison-static.net/public/html/site/img/homepage/jigsaw.svg"> I want to explore <br /><span>hobbies & interests</span>
                         <span className="arrow icon-carousel-arrow"></span>
                     </Link>
                 </div>
