2468621b6083f38c9daeefde97117bf456b1338b
Okiki Areo <okikiareo@gmail.com>
2023-08-22 05:08:51 +0100

    update on homepage

--- src/pages/home/landing-page/Testimonial.js
+++ src/pages/home/landing-page/Testimonial.js
@@ -2,13 +2,14 @@
 const  Testimonial = () => {
 return(
     <div className="testimonials">
+            
       <div className="inner">
-        <div className="testimonials__intro"><img className="lazyload desktop" width="257" height="155" data-srcset="https://cdn01.alison-static.net/public/html/site/img/homepage/trustpilot-banner.png, https://cdn01.alison-static.net/public/html/site/img/homepage/trustpilot-banner-2x.png 2x"
+        {/* <div className="testimonials__intro"><img className="lazyload desktop" width="257" height="155" data-srcset="https://cdn01.alison-static.net/public/html/site/img/homepage/trustpilot-banner.png, https://cdn01.alison-static.net/public/html/site/img/homepage/trustpilot-banner-2x.png 2x"
                 data-src="https://cdn01.alison-static.net/public/html/site/img/homepage/trustpilot-banner.png" title="Fhorge on TrustPilot" alt="Fhorge on TrustPilot" /> <img className="mobile lazyload" width="327" height="68" data-src="https://cdn01.alison-static.net/public/html/site/img/homepage/trustpilot-banner-mobile.svg"/>
-    </div>
-        <div
-            className="testimonial__slider">
-            <div className="testimonial__img"><img className="lazyload" height="169" width="324" data-srcset="https://cdn01.alison-static.net/public/html/site/img/homepage/Trustpilot1.png, https://cdn01.alison-static.net/public/html/site/img/homepage/Trustpilot1@2x.png 2x" data-src="https://cdn01.alison-static.net/public/html/site/img/homepage/Trustpilot1.png"
+  */}   </div>
+        <div className="testimonial__slider">
+           
+            {/* <div className="testimonial__img"><img className="lazyload" height="169" width="324" data-srcset="https://cdn01.alison-static.net/public/html/site/img/homepage/Trustpilot1.png, https://cdn01.alison-static.net/public/html/site/img/homepage/Trustpilot1@2x.png 2x" data-src="https://cdn01.alison-static.net/public/html/site/img/homepage/Trustpilot1.png"
                     title="Fhorge on TrustPilot" alt="Fhorge on TrustPilot" /></div>
             <div className="testimonial__img"><img className="lazyload" height="169" width="324" data-srcset="https://cdn01.alison-static.net/public/html/site/img/homepage/Trustpilot2.png, https://cdn01.alison-static.net/public/html/site/img/homepage/Trustpilot2@2x.png 2x" data-src="https://cdn01.alison-static.net/public/html/site/img/homepage/Trustpilot2.png"
                     title="Fhorge on TrustPilot" alt="Fhorge on TrustPilot" /></div>
@@ -26,9 +27,8 @@ return(
                     title="Fhorge on TrustPilot" alt="Fhorge on TrustPilot" /></div>
             <div className="testimonial__img"><img className="lazyload" height="169" width="324" data-srcset="https://cdn01.alison-static.net/public/html/site/img/homepage/Trustpilot9.png, https://cdn01.alison-static.net/public/html/site/img/homepage/Trustpilot9@2x.png 2x" data-src="https://cdn01.alison-static.net/public/html/site/img/homepage/Trustpilot9.png"
                     title="Fhorge on TrustPilot" alt="Fhorge on TrustPilot" /></div>
+   */} </div> 
     </div>
-    </div>
-</div> 
  )
 }
 export default Testimonial;
