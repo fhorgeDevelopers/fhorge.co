c3c6fdd6a1d9181d94795c77416a2af7bc8b6cd4
Okiki Areo <okikiareo@gmail.com>
2023-08-26 01:55:48 +0100

    home & earn page updated

--- src/pages/home/career/Career.js
+++ src/pages/home/career/Career.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import "../style/style.css"
-// import "./style/home.css";
-// import '../../../css/earn-&-build.css'
 import Navigation from '../../components/Navigation/Navigation';
 import CareerBody from './CareerBody';
 import Footer from '../../components/Footer/Footer';
 import Switch from '../Switch.js';
 import Careerss from "../landing-page/Careerss"
-
+import Goals from "../landing-page/Goals"
+import Goalsbelow from '../landing-page/Goalsbelow';
 const Career = () => {
     return (
         <>
-            <Navigation />
-            <Switch />
-            <CareerBody />
+            <Navigation/>
+            <Switch/>
+            <CareerBody/>
             <Careerss/>
+            <Goals/>
+            <Goalsbelow/>
             <Footer />
         </>
     )
