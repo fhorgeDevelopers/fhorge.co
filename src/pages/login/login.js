30f365ef741cf923e318a59ce19e2f730fac81a9
Okiki Areo <okikiareo@gmail.com>
2023-11-07 23:40:37 +0100

    initial login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,8 +1,25 @@
 
 const login = () => {
 return(
-    <div>
+    <div class="login-section flex">
+      <div class="login flex">
+        <div class="login-inner flex">
+      <div class="login-head flex">
+     <h3 class="sign flex">SIGN UP</h3>
+       <h3 class="log flex">LOGIN</h3>
+    </div>
+    <div class="login-info flex">
+    <h2>Welcome Back!</h2>
+    <p>Let's get you logged in</p>
+    <div class="sign-with flex">
+    <a href="" class="flex"><img src="img/google.png" alt="" class="google"></img> Login with Google</a>
+    <a href="" class="flex"><img src="img/facebook.png" alt="" class="facebook"></img>  Login with Facebook</a>
+    </div>
+    <div class="or">OR</div>
 
+        </div>
+        </div>
+    </div>
     </div>
 )
 }
