2d3e9e6f20a07634ab2ef66f1f53d79145a31c37
Okiki Areo <okikiareo@gmail.com>
2023-11-12 19:21:54 +0100

    commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import "./login/style.css";
+import Login from "./login/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const Home = () => {
     return (
-        <div>Fhorge Home</div>
+        <>
+          <Login/>
+        </>
+        // <div>Fhorge Home</div>
+       
     )
 }
 
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,6 +1,6 @@
+import { Link } from "react-router-dom";
 
-
-const login = () => {
+const Login = () => {
 return(
     <div class="login-section flex">
       <div class="login flex">
@@ -13,33 +13,33 @@ return(
     <h2>Welcome Back!</h2>
     <p>Let's get you logged in</p>
     <div class="sign-with flex">
-    <a href="" class="flex"><img src="img/google.png" alt="" class="google"></img> Login with Google</a>
-    <a href="" class="flex"><img src="img/facebook.png" alt="" class="facebook"></img>  Login with Facebook</a>
+    <Link to=" " class="flex"><img src="img/google.png" alt="" class="google"/>Login with Google</Link>
+    <Link to="" class="flex"><img src="img/facebook.png" alt="" class="facebook"/>  Login with Facebook</Link>
     </div>
     <div class="or">OR</div>
     <div class="form flex">
     <form action="">
         <div class="page-col col-up">
-            <input class="page-field email" type="email" name="email" id="" placeholder="Enter your Email"> </input>
+            <input class="page-field email" type="email" name="email" id="" placeholder="Enter your Email"/>
         </div>
         <div class="page-col">
-            <input class="page-field password" type="password" name="password" id="" placeholder="Create a password"> </input>
-            <img src="img/icon_hide.svg" alt="" class="page-toggle"></img>
+            <input class="page-field password" type="password" name="password" id="" placeholder="Create a password" />
+            <img src="img/icon_hide.svg" alt="" class="page-toggle"/>
         </div>
     </form>
 </div>
 <div  class="forgot flex">
-    <a href="">Forgot your password ?</a>
+    <Link to="">Forgot your password ?</Link>
 </div>
 
 <p class="click">By clicking “Get Started”, you agree to our Terms of Use and our Privacy Policy</p>
 
-<button href="" class="start-btn">Get Started</button>
-<p>Don’t have an account, <a href="" class="create-one">Create one</a></p>
+<button href="" class="start-btn flex">Login<img src="img/lock.png" alt="" class="lock"/></button>
+<p>Don’t have an account, <Link to="" class="create-one">Create one</Link></p>
         </div>
         </div>
     </div>
     </div>
 )
 }
-export default login;
+export default Login;
